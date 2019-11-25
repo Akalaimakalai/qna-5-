538632d34844b5aeab624ecaa3ab8a0addf4737f
Ar4i <jackshaakara@gmail.com>
2019-11-25 22:36:58 +0300

    little corrections

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -9,21 +9,21 @@ RSpec.describe Question, type: :model do
 
   describe '#sort_answers' do
     let(:question) { create(:question) }
-    let!(:answer1) { create(:answer, question: question, created_at: Time.now) }
-    let!(:answer2) { create(:answer, question: question, created_at: Time.now + 100) }
-    let!(:answer3) { create(:answer, question: question, created_at: Time.now - 100) }
+    let!(:answer1) { create(:answer, question: question) }
+    let!(:answer2) { create(:answer, question: question) }
+    let!(:answer3) { create(:answer, question: question) }
 
     context 'if best answer presents' do
-      let!(:answer) { create(:answer, question: question, correct: true, created_at: Time.now + 300) }
+      let!(:answer) { create(:answer, question: question, correct: true) }
 
       it 'it return list of answers sorteted with best - first' do
-        expect(question.sort_answers).to eq [answer, answer3, answer1, answer2]
+        expect(question.sort_answers).to eq [answer, answer1, answer2, answer3]
       end
     end
 
     context 'if is no best answer here' do
       it 'return sorted list of answrs sorted by created date' do
-        expect(question.sort_answers).to eq [answer3, answer1, answer2]
+        expect(question.sort_answers).to eq [answer1, answer2, answer3]
       end
     end
   end
