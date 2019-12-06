fa3d1909919f0aab21a2db62d9c0230ebbf36fc5
Ar4i <jackshaakara@gmail.com>
2019-12-06 15:35:34 +0300

    prepare views

--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -1,5 +1,18 @@
 $(document).on('turbolinks:load', function() {
   eventForEditQuestion();
+
+  var medal = $('.new-medal-link');
+
+  if (medal) {
+    medal.on('click', function(e) {
+      e.preventDefault();
+      $(this).hide();
+  
+      var questionId = $(this).data('questionId');
+  
+      $('new-medal-question-' + questionId).removeClass('hidden');
+    });
+  };
 });
 
 function eventForEditQuestion() {
