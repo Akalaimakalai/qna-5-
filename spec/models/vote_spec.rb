b5c829f7f13680efb90d4d643fd9bd926fda9554
Ar4i <jackshaakara@gmail.com>
2019-12-25 11:58:42 +0300

    final corrections

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -5,4 +5,17 @@ RSpec.describe Vote, type: :model do
   it { should belong_to :user }
   it { should validate_presence_of :value }
   it { should validate_inclusion_of(:value).in_array([ 1, -1 ]) }
+
+  describe 'before_create' do
+    let(:user) { create(:user) }
+    let(:question) { create(:question) }
+    let!(:vote) { create(:vote, user: user, votable: question) }
+
+    it 'deletes previous votes of user' do
+      new_vote = create(:vote, user: user, votable: question)
+
+      expect(user.votes).to_not include(vote)
+      expect(user.votes).to include(new_vote)
+    end
+  end
 end
