10d0685471be8e2d2a417fb5123963c55e93174f
Ar4i <jackshaakara@gmail.com>
2020-01-18 02:30:34 +0300

    install cancancan gem

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,6 +4,13 @@ ENV['RAILS_ENV'] ||= 'test'
 
 require File.expand_path('../config/environment', __dir__)
 
+# ===MY REQUIRES===
+
+# authorization
+require 'cancan/matchers'
+
+# ---end---
+
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
