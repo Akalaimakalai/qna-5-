66139401576fa56525e266e5697b89b3a07840bf
Ar4i <jackshaakara@gmail.com>
2020-01-22 05:32:51 +0300

    API returns list of users

--- spec/api/v1/profiles_spec.rb
+++ spec/api/v1/profiles_spec.rb
@@ -34,4 +34,32 @@ describe 'Profiles API', type: :request do
       end
     end
   end
+
+  describe 'GET /api/v1/profiles/all' do
+
+    it_behaves_like 'API Authorizable' do
+      let(:method) { :get }
+      let(:api_path) { '/api/v1/profiles' }
+    end
+
+    context 'authorized' do
+      let!(:users) { create_list(:user, 3)}
+      let(:user) { users.first }
+      let(:access_token) { create(:access_token, resource_owner_id: user.id) }
+
+      before { get '/api/v1/profiles', params: { access_token: access_token.token }, headers: headers }
+
+      it 'returns 200 status' do
+        expect(response).to be_successful
+      end
+
+      it 'returns list of users' do
+        expect(json['users'].size).to eq 2
+      end
+
+      it 'does not include current user' do
+        expect(json['users']).to_not be_include(user)
+      end
+    end
+  end
 end
