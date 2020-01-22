dfece3db7a4269cae108f5b32f78494160143f5c
Ar4i <jackshaakara@gmail.com>
2020-01-22 20:33:03 +0300

    return amazon test server

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,14 @@
 class ApplicationController < ActionController::Base
-  before_action :set_host_for_local_storage
+  # before_action :set_host_for_local_storage
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, alert: exception.message
   end
 
-  private
+  # private
 
-  def set_host_for_local_storage
-    env_now ||= Rails.application.config.active_storage.service
-    ActiveStorage::Current.host ||= request.base_url if env_now == :test
-  end
+  # def set_host_for_local_storage
+  #   env_now ||= Rails.application.config.active_storage.service
+  #   ActiveStorage::Current.host ||= request.base_url if env_now == :test
+  # end
 end
