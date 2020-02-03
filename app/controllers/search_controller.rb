b16f22163b0c7f1594a42fdd24c2f55dcd8db43e
Ar4i <jackshaakara@gmail.com>
2020-02-03 06:47:46 +0300

    add searching by category

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,19 @@
 class SearchController < ApplicationController
+  CLASSES = {
+    "Везде" => nil,
+    "Вопросы" => Question,
+    "Ответы" => Answer,
+    "Комментарии" => Comment,
+    "Пользователи" => User
+}.freeze
 
   def index
-    @results = ThinkingSphinx.search(params[:search])
+    @results = ThinkingSphinx.search(params[:search], classes: [find_class])
+  end
+
+  private
+
+  def find_class
+    CLASSES[params[:classes]]
   end
 end
